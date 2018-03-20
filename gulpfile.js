fe40b178da33d05890e4b6f8b06ac8f356f3fe4c
Anton Morya <anton.morya@gmail.com>
2018-03-20 22:43:44 +0200

    Initial commit with initial work

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require("gulp-rename");
+var combineMq = require('gulp-combine-mq');
+
+
+//autoprefixer
+gulp.task('aprefix', () =>
+    gulp.src('src/app.css')
+        .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(combineMq({
+            beautify: false
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 15 versions']
+        }))
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('combineMq', function () {
+    return gulp.src('./css/main.min.css')
+        .pipe(combineMq({
+            beautify: false
+        }))
+        .pipe(gulp.dest('tmp'));
+});
+
+
+// watch files for changes and reload
+gulp.task('serve', function () {
+    gulp.watch('./sass/**/*.scss', gulp.parallel('sass'));
+});
+

4b2230f09e0217122b4d7dd1f450ad85a02fc531
Anton Morya <anton.morya@gmail.com>
2018-03-22 20:42:55 +0200

    Added: - React.js gallery widget + modals - In-page navigation - Responsive style improvements

---
+++ images.js
@@ -0,0 +1,76 @@
+window.images = [
+    {
+        "src": "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+        "thumbnail": "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
+        "thumbnailWidth": 320,
+        "thumbnailHeight": 174,
+        "caption": "After Rain (Jeshu John - designerspics.com)",
+        "field1": 'Custom field 1',
+        "field2": 'Custom field 2',
+        "field3": 'Custom field 3',
+        "field4": 'Custom field 4',
+        "field5": 'price'
+    },
+    {
+        "src": "https://picsum.photos/800/800/?random",
+        "thumbnail": "https://picsum.photos/200/200/?random",
+        "thumbnailWidth": 200,
+        "thumbnailHeight": 200,
+        "caption": "Boats (Jeshu John - designerspics.com)",
+        "field1": 'Custom field 1',
+        "field2": 'Custom field 2',
+        "field3": 'Custom field 3',
+        "field4": 'Custom field 4',
+        "field5": 'price'
+    },
+    {
+        "src": "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+        "thumbnail": "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
+        "thumbnailWidth": 320,
+        "thumbnailHeight": 212,
+        "caption": "pencils",
+        "field1": 'Custom field 1',
+        "field2": 'Custom field 2',
+        "field3": 'Custom field 3',
+        "field4": 'Custom field 4',
+        "field5": 'price'
+    },
+    {
+        "src": "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+        "thumbnail": "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
+        "thumbnailWidth": 320,
+        "thumbnailHeight": 174,
+        "caption": "After Rain",
+        "field1": 'Custom field 1',
+        "field2": 'Custom field 2',
+        "field3": 'Custom field 3',
+        "field4": 'Custom field 4',
+        "field5": 'price'
+    },
+    {
+        "src": "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+        "thumbnail": "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+        "thumbnailWidth": 320,
+        "thumbnailHeight": 212,
+        "caption": "Boats",
+        "field1": 'Custom field 1',
+        "field2": 'Custom field 2',
+        "field3": 'Custom field 3',
+        "field4": 'Custom field 4',
+        "field5": 'price'
+    },
+    {
+        "src": "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+        "thumbnail": "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
+        "thumbnailWidth": 320,
+        "thumbnailHeight": 212,
+        "caption": "pencils again",
+        "field1": 'Custom field 1',
+        "field2": 'Custom field 2',
+        "field3": 'Custom field 3',
+        "field4": 'Custom field 4',
+        "field5": 'price'
+    }
+]
+
+console.log(window.images)
